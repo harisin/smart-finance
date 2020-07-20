81a461c509c9509f3358d53151ce487a62ee0ebd
singhfs <harish.singh1@rbs.co.uk>
2020-07-20 21:23:42 +0530

    Sample code

---
+++ packages/ui-common/lib/Header.js
@@ -0,0 +1,18 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = Header;
+
+var _react = _interopRequireDefault(require("react"));
+
+require("./common.css");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+function Header() {
+  return /*#__PURE__*/_react["default"].createElement("div", {
+    className: "header"
+  }, /*#__PURE__*/_react["default"].createElement("h2", null, "#APIthon"));
+}
